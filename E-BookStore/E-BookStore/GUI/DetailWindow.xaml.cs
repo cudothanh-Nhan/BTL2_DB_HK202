3002864a1d634579921f7ac360f23a8539a9c817
ttl <lamtoan557@gmail.com>
2021-05-28 19:01:26 +0700

    Detail.XAML

--- E-BookStore/E-BookStore/GUI/DetailWindow.xaml.cs
+++ E-BookStore/E-BookStore/GUI/DetailWindow.xaml.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using E_BookStore.BLL;
+using E_BookStore.DTO;
 
 namespace E_BookStore.GUI
 {
-    class DetailWindow
+    /// <summary>
+    /// Interaction logic for MainUIWindow.xaml
+    /// </summary>
+    public partial class DetailWindow : Window
     {
+
+        public static Image getImage(string url)
+        {
+            var image = new Image();
+            var fullFilePath = url;
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+            bitmap.EndInit();
+            image.Source = bitmap;
+            image.Stretch = Stretch.UniformToFill;
+            return image;
+        }
+        MainUIBLL bll;
     }
 }
