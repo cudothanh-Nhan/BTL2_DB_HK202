c339cb12dd3f3d83903c83bf3fd2aac8e0e9031f
ttl <lamtoan557@gmail.com>
2021-06-01 11:34:47 +0700

    But quantity

--- E-BookStore/E-BookStore/BLL/DetailBLL.cs
+++ E-BookStore/E-BookStore/BLL/DetailBLL.cs
@@ -1,4 +1,5 @@
 ﻿using E_BookStore.DAO;
+using E_BookStore.DTO;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,8 @@ namespace E_BookStore.BLL
         private DetailDAO dao;
         public DetailBLL()
         {
-            dao = new DetailDAO("localhost", 3306, "db", "root", "lekhiettoan1");
+            SqlConnectBLL sql = new SqlConnectBLL();
+            dao = new DetailDAO(sql.host, sql.port, sql.database, sql.username, sql.password);
         }
         public bool checkQuantity(string quantity)
         {
@@ -42,28 +44,48 @@ namespace E_BookStore.BLL
         }
         public int UpdatePPartOf(int P_Product_ID, int Order_quantity, int Or_cus_ID)
         {
-            MySqlCommand cmdEmail = dao.CheckProExist(P_Product_ID);
+            MySqlCommand cmdEmail = dao.CheckProExist(P_Product_ID, Or_cus_ID);
             MySqlDataAdapter adapter = new MySqlDataAdapter();
             adapter.SelectCommand = cmdEmail;
             DataSet dataSet = new DataSet();
             adapter.Fill(dataSet);
             if (dataSet.Tables[0].Rows.Count > 0)
             {
-                dao.UpdateOrder(P_Product_ID, Order_quantity);
+                dao.UpdateOrder(P_Product_ID, Order_quantity, Or_cus_ID);
             }
             else
             {
-                dao.InsertPro(P_Product_ID, Order_quantity,Or_cus_ID);
+                dao.InsertPro(P_Product_ID, Order_quantity, Or_cus_ID);
             }
             return dao.getProCate(Or_cus_ID);
         }
         public int ShowTotalProCate(int Or_cus_ID)
-        { 
+        {
             return dao.getProCate(Or_cus_ID);
         }
         public void InsertPPartOf(int P_Product_ID, int Order_quantity, int Or_cus_ID)
         {
             dao.InsertOrder(P_Product_ID, Order_quantity, Or_cus_ID);
         }
+
+        public string checkType(int proId)
+        {
+            List<string> listBook = dao.getAllBookID();
+            string id = proId.ToString();
+            foreach (var i in listBook)
+            {
+                if (i == id) return "Book";
+            }
+            return "Magazine";
+        }
+        public Book getBookDetail(int proId)
+        {
+            return dao.getBookDetail(proId);
+        }
+        public Magazine getMagaDetail(int proId)
+        {
+            return dao.getMagaDetail(proId);
+        }
+
     }
 }
--- E-BookStore/E-BookStore/DAO/DetailDAO.cs
+++ E-BookStore/E-BookStore/DAO/DetailDAO.cs
@@ -1,8 +1,10 @@
-﻿using MySql.Data.MySqlClient;
+﻿using E_BookStore.DTO;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,21 +29,23 @@ namespace E_BookStore.DAO
         {
             MySqlConnection con = new MySqlConnection(connString);
             con.Open();
-            MySqlCommand cmdCusID = new MySqlCommand("Select * from ORDERS,Status where Or_cus_ID=" + Customer_ID.ToString() + " and Sta_Order_ID = Order_ID and Status = 'onCart';", con);
+            MySqlCommand cmdCusID = new MySqlCommand("Select * from ORDERS,STATUS where Or_cus_ID=" + Customer_ID.ToString() + " and Sta_Order_ID = Order_ID and Status ='onCart';", con);
             return cmdCusID;
         }
-        public MySqlCommand CheckProExist(int P_Product_ID)
+        public MySqlCommand CheckProExist(int P_Product_ID, int Order_Cus_ID)
         {
             MySqlConnection con = new MySqlConnection(connString);
             con.Open();
-            MySqlCommand cmdEmail = new MySqlCommand("Select * from P_PART_OF where P_Product_ID=" + P_Product_ID.ToString() + ";", con);
+            MySqlCommand cmdEmail = new MySqlCommand("Select * from P_PART_OF,ORDERS,Status where P_Product_ID=" + P_Product_ID.ToString()
+                + " and P_Order_ID = Order_ID and Sta_Order_ID = Order_ID and Status = 'onCart' and Or_Cus_ID = " + Order_Cus_ID.ToString() + ";", con);
             return cmdEmail;
         }
-        public void UpdateOrder(int P_Product_ID, int Order_quantity)
+        public void UpdateOrder(int P_Product_ID, int Order_quantity, int Order_Cus_ID)
         {
             MySqlConnection con = new MySqlConnection(connString);
             con.Open();
-            string Query1 = "Update P_PART_OF SET Order_quantity =Order_quantity + " + Order_quantity.ToString() + " WHERE P_Product_ID =" + P_Product_ID.ToString() + ";";
+            int x = this.getOrderIDD(Order_Cus_ID);
+            string Query1 = "Update P_PART_OF SET Order_quantity = Order_quantity + " + Order_quantity.ToString() + " WHERE P_Product_ID = " + P_Product_ID.ToString() + " and P_Order_ID = " + x.ToString() + ";";
             MySqlCommand cmd1 = new MySqlCommand(Query1, con);
             cmd1.CommandType = CommandType.Text;
             cmd1.ExecuteNonQuery();
@@ -57,7 +61,13 @@ namespace E_BookStore.DAO
             MySqlCommand cmd2 = new MySqlCommand(Query2, con);
             cmd2.CommandType = CommandType.Text;
             cmd2.ExecuteNonQuery();
-            int x = this.getOrderID(Or_cus_ID);
+            MySqlCommand cmdcdtn = new MySqlCommand("select last_insert_id();", con);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = cmdcdtn;
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet);
+            int x = dataSet.Tables[0].Rows.Count;
+            //MessageBox.Show(x.ToString());
             string Query1 = "Insert into P_PART_OF(P_Order_ID,P_Product_ID,Order_quantity) values (" + x.ToString() + ","
                 + P_Product_ID.ToString() + "," +
                 Order_quantity.ToString() + ");";
@@ -65,19 +75,19 @@ namespace E_BookStore.DAO
             cmd1.CommandType = CommandType.Text;
             cmd1.ExecuteNonQuery();
             string Query3 = "Insert into STATUS(Sta_Order_ID,Status) values (" +
-                x.ToString() +"," + "'onCart'" + ");";
+                x.ToString() + "," + "'onCart'" + ");";
             MySqlCommand cmd3 = new MySqlCommand(Query3, con);
             cmd3.CommandType = CommandType.Text;
             cmd3.ExecuteNonQuery();
             con.Close();
         }
-        
+
         public void InsertPro(int P_Product_ID, int Order_quantity, int Or_cus_ID)
         {
             MySqlConnection con = new MySqlConnection(connString);
             con.Open();
-            int x = this.getOrderID(Or_cus_ID);
-            string Query1 = "Insert into P_PART_OF(P_Order_ID,P_Product_ID,Order_quantity) values (" +x.ToString()+","+
+            int x = this.getOrderIDD(Or_cus_ID);
+            string Query1 = "Insert into P_PART_OF(P_Order_ID,P_Product_ID,Order_quantity) values (" + x.ToString() + "," +
                 P_Product_ID.ToString() + "," +
                 Order_quantity.ToString() + ");";
             MySqlCommand cmd1 = new MySqlCommand(Query1, con);
@@ -88,6 +98,36 @@ namespace E_BookStore.DAO
         private int findIndex(string[] arr, string key)
         {
             return Array.FindIndex<String>(arr, element => element.ToUpper() == key.ToUpper());
+        }
+        public int getOrderIDD(int Cus_Id)
+        {
+            var conn = new MySqlConnection(connString);
+            int res = 0;
+            try
+            {
+                conn.Open();
+                string sqlStatement = "call UpdateProduct(" + Cus_Id.ToString() + ");";
+                var cmd = new MySqlCommand(sqlStatement, conn);
+                var reader = cmd.ExecuteReader();
+
+                string[] columnName = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    columnName[i] = reader.GetName(i);
+                }
+                while (reader.Read())
+                {
+
+                    res = reader.GetInt32(findIndex(columnName, "Order_ID"));
+
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            return res;
         }
 
         public int getProCate(int Cus_Id)
@@ -120,6 +160,7 @@ namespace E_BookStore.DAO
             }
             return res;
         }
+
         public int getOrderID(int Cus_Id)
         {
             var conn = new MySqlConnection(connString);
@@ -127,7 +168,7 @@ namespace E_BookStore.DAO
             try
             {
                 conn.Open();
-                string sqlStatement = "call OrderIDuse2(" + Cus_Id.ToString() + ");";
+                string sqlStatement = "call OrderIDuse(" + Cus_Id.ToString() + ");";
                 var cmd = new MySqlCommand(sqlStatement, conn);
                 var reader = cmd.ExecuteReader();
 
@@ -150,6 +191,147 @@ namespace E_BookStore.DAO
             }
             return res;
         }
-        
+
+        public Book getBookDetail(int bookId)
+        {
+            var conn = new MySqlConnection(connString);
+            Book book = new Book();
+            try
+            {
+                conn.Open();
+                string sqlStatement = "call getBookDetail(" + bookId.ToString() + ");";
+                var cmd = new MySqlCommand(sqlStatement, conn);
+                var reader = cmd.ExecuteReader();
+
+                string[] columnName = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    columnName[i] = reader.GetName(i);
+                }
+                while (reader.Read())
+                {
+                    book.Id = reader.GetInt32(findIndex(columnName, "Product_ID"));
+                    book.ImgUrl = reader.GetString(findIndex(columnName, "imgUrl"));
+                    book.Name = reader.GetString(findIndex(columnName, "name"));
+                    book.Quantiy = reader.GetInt32(findIndex(columnName, "sum(Quantity)"));
+                    book.Price = reader.GetInt32(findIndex(columnName, "price"));
+                    Store store = new Store();
+                    store.Street = reader.GetString(findIndex(columnName, "Sto_Street"));
+                    store.City = reader.GetString(findIndex(columnName, "Sto_City"));
+                    book.Store = store;
+                    book.Language = reader.GetString(findIndex(columnName, "Language"));
+                    book.Publisher = reader.GetString(findIndex(columnName, "Publisher"));
+                    DateTime date = DateTime.ParseExact(reader.GetString(findIndex(columnName, "Publish_year")), "yyyy", CultureInfo.InvariantCulture);
+                    book.PublishYear = date;
+                    book.NPage = reader.GetInt32(findIndex(columnName, "Pages"));
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            return book;
+        }
+        public Magazine getMagaDetail(int magaId)
+        {
+            var conn = new MySqlConnection(connString);
+            Magazine maga = new Magazine();
+            try
+            {
+                conn.Open();
+                string sqlStatement = "call getMagazineDetail(" + magaId.ToString() + ");";
+                var cmd = new MySqlCommand(sqlStatement, conn);
+                var reader = cmd.ExecuteReader();
+
+                string[] columnName = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    columnName[i] = reader.GetName(i);
+                }
+                while (reader.Read())
+                {
+                    MagazineSeri seri = new MagazineSeri();
+                    maga.Id = reader.GetInt32(findIndex(columnName, "Product_ID"));
+                    maga.ImgUrl = reader.GetString(findIndex(columnName, "imgUrl"));
+                    seri.Name = reader.GetString(findIndex(columnName, "Name"));
+                    seri.Publisher = reader.GetString(findIndex(columnName, "Publisher"));
+                    maga.SeriName = seri;
+                    maga.No = reader.GetInt32(findIndex(columnName, "NO"));
+                    maga.Quantiy = reader.GetInt32(findIndex(columnName, "sum(Quantity)"));
+                    maga.Price = reader.GetInt32(findIndex(columnName, "price"));
+                    Store store = new Store();
+                    store.Street = reader.GetString(findIndex(columnName, "Sto_Street"));
+                    store.City = reader.GetString(findIndex(columnName, "Sto_City"));
+                    maga.Store = store;
+                    maga.Language = reader.GetString(findIndex(columnName, "Language"));
+                    DateTime date = DateTime.ParseExact(reader.GetString(findIndex(columnName, "Publish_date")), "M/d/yyyy hh:mm:ss tt", CultureInfo.InvariantCulture);
+                    maga.PublishDate = date;
+                }
+
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            return maga;
+        }
+        public List<string> getAllBookID()
+        {
+            var conn = new MySqlConnection(connString);
+            List<string> proID = new List<string>();
+            try
+            {
+                conn.Open();
+                string sqlStatement = "call getAllBookID();";
+                var cmd = new MySqlCommand(sqlStatement, conn);
+                var reader = cmd.ExecuteReader();
+
+                string[] columnName = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    columnName[i] = reader.GetName(i);
+                }
+                while (reader.Read())
+                {
+                    proID.Add(reader.GetString(findIndex(columnName, "Book_Pro_ID")));
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            return proID;
+        }
+        public List<string> getAllMagaID()
+        {
+            var conn = new MySqlConnection(connString);
+            List<string> proID = new List<string>();
+            try
+            {
+                conn.Open();
+                string sqlStatement = "call getAllMagazineID();";
+                var cmd = new MySqlCommand(sqlStatement, conn);
+                var reader = cmd.ExecuteReader();
+
+                string[] columnName = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    columnName[i] = reader.GetName(i);
+                }
+                while (reader.Read())
+                {
+                    proID.Add(reader.GetString(findIndex(columnName, "Maga_Pro_ID")));
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.ToString());
+            }
+            return proID;
+        }
     }
 }
